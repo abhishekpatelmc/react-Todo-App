d49645448a0ad04b981da7dbd308a8d6cc46568c
abhishekpatelmc <mcabhishekpatel2017@gmail.com>
2021-03-29 22:24:48 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+//Importing Components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-     
+      <header>
+        <h1>react Todo List </h1>
+      </header>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+const Todo = () => {
+  return (
+    <div className="todo">
+      <li className="todo-item">HEYY</li>
+      <button className="complete-btn">
+        <i className="fas fas-check"></i>
+      </button>
+      <button className="trash-btn">
+        <i className="fas fas-trash"></i>
+      </button>
+    </div>
+  );
+};
+
+export default Todo;
